3b5d677298b15fbcdc0888a74ab87ccf6eb773b6
Kenneth Brook <kenneth.brook@hotmail.com>
2022-03-05 13:57:23 -0500

    data files started

--- src/components/attractions/Attractions.js
+++ src/components/attractions/Attractions.js
@@ -3,7 +3,7 @@ import React from "react";
 function Attractions() {
     return (
         <div className="attractionsWrap">
-            <h1>attractions</h1>
+            <h1>Attractions</h1>
         </div>
     )
 }
---
+++ src/data/attraction.js
@@ -0,0 +1,17 @@
+export const attractionData = [
+    {
+        id:1,
+        name:"The Pinball Palace",
+        street:"3303 Hopkins Ave",
+        city:"Brunswick",
+        state:"GA",
+        zip:"31520",
+        img:"",
+        lat:"31.1697287",
+        long:"-81.4757067"
+    }
+];
+
+export default {
+    attractionData,
+}
